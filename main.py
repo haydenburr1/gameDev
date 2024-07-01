65cbfc9c3b568db75960490869a680bdee0d7a1b
haydenburr1 <155483992+haydenburr1@users.noreply.github.com>
2024-07-01 14:36:06 +0100

    added comments

--- main.py
+++ main.py
@@ -4,30 +4,43 @@ import sys
 
 class App:
     def __init__(self):
-        pg.init()
+        pg.init() 
+        
+        # variables for screen creation
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.font = pg.font.Font(None, 50)
+        
+        
+        # variables for blitting
+        self.font_surf = self.font.render("hello world", False, (255, 255, 255))
+        self.font_rect = self.font_surf.get_rect(center=(HALF_RES))
        
     def game_loop(self):
+        # basic game loop
+        # TODO: controls for when neded 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-    
-
+                
     def draw(self):
-        pass
+        # blitting to the screen. avoid anything else in this method 
+        self.screen.blit(self.font_surf, self.font_rect)
     
     def update(self):
+        # for updating rects and other variables, do it here 
         pg.display.update()
         self.clock.tick(FPS)
         
     def run(self):
         while True:
+            # add functions as per...
             self.game_loop()
             self.draw()
             self.update()
-    
+
+# needed to run
 if __name__ == "__main__":
     app = App()
     app.run()
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-
+# add any const variables here. saves cluttering the main file
 RES = WIDTH, HEIGHT = 800, 600
+HALF_RES = HALF_WIDTH, HALF_HEIGHT = WIDTH / 2, HEIGHT / 2
 
 FPS = 60
