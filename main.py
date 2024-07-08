44f09e24dd363b5452574866afb1ea07aaace769
haydenburr1 <155483992+haydenburr1@users.noreply.github.com>
2024-07-08 10:52:30 +0100

    attempted collisions

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ class App:
         self.planet_surf = pg.image.load("assets/planet2.png").convert_alpha()
         self.planet_rect = self.planet_surf.get_rect(center=HALF_RES)
 
-    @staticmethod
-    def game_loop():
+    def game_loop(self):
         # basic game loop
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -40,9 +39,16 @@ class App:
     def collisions(self):
         if self.planet_rect.colliderect(self.player.rect):
             self.player.gravity = 0
-        
-        #pg.sprite.spritecollide(self.player, self.enemy_list, True)
 
+        # both these methods dont work. 
+        """
+        for enemy in self.enemy_list:
+            if self.player.rect.colliderect(enemy.rect):
+                self.enemy_list.remove(enemy)
+        
+        self.enemy_list = [enemy for enemy in self.enemy_list if not self.player.rect.colliderect(enemy.rect)]
+        """
+        
     def update(self):
         # for updating rects and other variables, do it here 
         pg.display.update()
